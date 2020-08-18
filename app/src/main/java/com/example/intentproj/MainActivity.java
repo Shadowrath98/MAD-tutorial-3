b69b09481cd85dc9641956dd124ff12c0633bed2
SHADOWRATH <etharuw@gmail.com>
2020-08-18 23:50:47 +0530

    Second commit

--- app/src/main/java/com/example/intentproj/MainActivity.java
+++ app/src/main/java/com/example/intentproj/MainActivity.java
@@ -5,9 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +21,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        LayoutInflater li = getLayoutInflater();
+
+        View layout = li.inflate(R.layout.customtoast1, (ViewGroup)
+                findViewById(R.id.custom_toast_layout));
+
+        Toast toast = new Toast(getApplicationContext());
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+        toast.setView(layout);//setting the view of custom toast layout
+        toast.show();
+
 
         btn = findViewById(R.id.OK);
         btn.setOnClickListener(new View.OnClickListener() {
